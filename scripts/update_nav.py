0e38cc1af6a5cc7582f54939e950a3174919856c
Desiderio Romero <dr3@Desiderios-MacBook-Air.local>
2026-03-08 22:37:03 -0500

    Add Biometeorological Feedback Kiosk (Student Wellbeing & Thermal Comfort Survey) page with real-time zone data logging

--- scripts/update_nav.py
+++ scripts/update_nav.py
@@ -3,7 +3,7 @@ import re
 
 nav_html = """
     <nav class="app-nav">
-        <a class="nav-link" href="campuses.html">Select Campus</a><span style="color: #ccc; padding: 6px;">|</span><a class="nav-link" href="index.html">Dashboard</a>
+        <a class="nav-link" href="campuses.html">Select Campus</a><span style="color: #ccc; padding: 6px;">|</span><a class="nav-link" href="dashboard_map.html">Dashboard</a>
         <a class="nav-link" href="canopy.html">Canopy Over Time</a>
         <a class="nav-link" href="trails.html">Trail Analysis</a>
         <a class="nav-link" href="wind.html">Wind Analysis</a>
@@ -11,12 +11,16 @@ nav_html = """
         <a class="nav-link" href="temperature.html">Temp Modeling</a>
         <a class="nav-link" href="soil_moisture.html">Soil Moisture</a>
         <a class="nav-link" href="albedo.html">Albedo</a>
+        <a class="nav-link" href="zoning.html">Site Zoning</a>
         <a class="nav-link" id="navData" href="data_pipeline.html">Data Telemetry</a>
         <a class="nav-link" href="3d-vision.html">3D Vision Concept</a>
         <a class="nav-link" href="school.html">School Profile</a>
         <a class="nav-link" href="biodiversity.html">Biodiversity</a>
         <a class="nav-link" href="protocols.html">Protocols</a>
-        <a class="nav-link" href="community_impact.html">Community Impact</a>
+        <a class="nav-link" href="index.html">Community Impact</a>
+        <a class="nav-link" href="student_tracking.html">Student Tracking</a>
+        <a class="nav-link" href="tree_3d.html">3D Shade Models</a>
+        <a class="nav-link" href="survey.html">Wellbeing Survey</a>
     </nav>
 """
 
